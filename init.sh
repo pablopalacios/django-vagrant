401eec8e9dba58b9cf70d0926a1ea4aff92b9291
Pablo Palácios <_@pablopalacios.me>
2016-04-13 17:24:10 -0300

    add ius repository provision

--- init.sh
+++ init.sh
@@ -3,3 +3,7 @@ yum -y install epel-release
 yum -y install nginx
 systemctl enable nginx
 systemctl start nginx
+
+yum -y install wget
+wget https://centos7.iuscommunity.org/ius-release.rpm -O /tmp/ius.rpm
+rpm -Uvh /tmp/ius.rpm
