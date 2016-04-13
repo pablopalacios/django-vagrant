c98db4108880ec81f1f31c0530388547559edee7
Pablo Palácios <_@pablopalacios.me>
2016-04-13 17:33:45 -0300

    added documentations into provision script

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
-yum -y install epel-release
+# epel is necessary for IUS repository and other packages (eg. nginx)
+# wget is used to grab things from web (eg. ius.rpm)
+yum -y install epel-release wget
 
+# IUS is necessary for Python 3.5.
+wget https://centos7.iuscommunity.org/ius-release.rpm -O /tmp/ius.rpm
+rpm -Uvh /tmp/ius.rpm
+
+# A HTTP server
 yum -y install nginx
 systemctl enable nginx
 systemctl start nginx
 
-yum -y install wget
-wget https://centos7.iuscommunity.org/ius-release.rpm -O /tmp/ius.rpm
-rpm -Uvh /tmp/ius.rpm
+# Python 3.5 and pip. Devel package is necessary when using some packages
 yum -y install python35u python35u-devel python35u-pip
