81e8f2242189df9876943ac68640802219240a56
Pablo Palácios <_@pablopalacios.me>
2016-04-14 18:33:17 -0300

    improved sed provision scripts

--- vagrant/django.sh
+++ vagrant/django.sh
@@ -15,10 +15,10 @@ echo -e "\nLOCALE_PATHS = [os.path.join(BASE_DIR, '../locale/')]" >> settings.py
 echo -e "\nFIXTURE_DIRS = [os.path.join(BASE_DIR, '../fixtures/')]" >> settings.py
 
 # configures database connection
-sed -i  "s/django.db.backends.sqlite3/django.db.backends.postgresql_psycopg2/" settings.py
-sed -i  "s/os.path.join(BASE_DIR, 'db.sqlite3')/'vagrant'/" settings.py
-sed -ie "/backends.postgresql/ a\        'PASSWORD': 'vagrant'," settings.py
-sed -ie "/backends.postgresql/ a\        'USER': 'vagrant'," settings.py
+sed -i "s/django.db.backends.sqlite3/django.db.backends.postgresql_psycopg2/" settings.py
+sed -i "s/os.path.join(BASE_DIR, 'db.sqlite3')/'vagrant'/" settings.py
+sed -i "/backends.postgresql/ a\        'PASSWORD': 'vagrant'," settings.py
+sed -i "/backends.postgresql/ a\        'USER': 'vagrant'," settings.py
 
 # runs first migration
 cd ~/src
