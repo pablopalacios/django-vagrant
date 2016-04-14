947a5b3532791778a452b120b5f17ef4e4a441d4
Pablo Palácios <_@pablopalacios.me>
2016-04-14 15:38:07 -0300

    fix postgresql user and database name

--- vagrant/postgresql.sh
+++ vagrant/postgresql.sh
@@ -5,7 +5,7 @@ postgresql-setup initdb
 sed -i 's/ident/md5/' /var/lib/pgsql/data/pg_hba.conf
 systemctl enable postgresql
 systemctl start postgresql
-## prefix db_ is used to facilitate deploy scripts replacements
-sudo -u postgres psql -c "CREATE USER db_user WITH PASSWORD 'db_password';"
-sudo -u postgres psql -c "CREATE DATABASE db_name;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE db_name TO db_user;";
+## creates user and database
+sudo -u postgres psql -c "CREATE USER vagrant WITH PASSWORD 'vagrant';"
+sudo -u postgres psql -c "CREATE DATABASE vagrant;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE vagrant TO vagrant;";
